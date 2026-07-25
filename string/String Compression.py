a627c8670a4cc9bed92898cf4c07a294c1d0a55f
shrutigarud07 <shrutigarud45@gmail.com>
2026-07-25 15:33:43 +0530

    Create String Compression.py

---
+++ string/String Compression.py
@@ -0,0 +1,13 @@
+string = input("Enter a string: ")
+
+result = ""
+count = 1
+
+for i in range(len(string)):
+    if i < len(string) - 1 and string[i] == string[i + 1]:
+        count += 1
+    else:
+        result += string[i] + str(count)
+        count = 1
+
+print("Compressed string:", result)
