89e2af1c70711e770aaecebe060322fc3fad5a4d
shrutigarud07 <shrutigarud45@gmail.com>
2026-08-14 00:32:04 +0530

    Create Sort Without sort().py

---
+++ Lists/Sort Without sort().py
@@ -0,0 +1,8 @@
+numbers = [5, 2, 8, 1, 3]
+
+for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+        if numbers[i] > numbers[j]:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+print("Sorted list:", numbers)
